995e1dba5ded2373454721a9ce4c1547dd0805e9
theobaseggio-cyber <theobaseggio@gmail.com>
2026-03-30 14:35:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 import fastify from 'fastify'
 import { DatabaseMemory } from './database-memory.js'
-import fastify from 'fastify'
-import { DatabaseMemory } from './database-memory.js'
 import staticPlugin from '@fastify/static'
 import cors from '@fastify/cors'
 import { fileURLToPath } from 'url'
@@ -12,45 +10,43 @@ const server = fastify()
 
 await server.register(cors)
 await server.register(staticPlugin, { root: __dirname, prefix: '/' })
-const server = fastify()
 
 const database = new DatabaseMemory()
+
 server.get('/', (req, reply) => {
-  return reply.redirect('/videos')
+  return reply.redirect('/index.html')
 })
+
 server.post('/videos', (req, reply) => {
-    const { title, description, duration } = req.body
-
-    database.create({ title, description, duration })
-
-    return reply.status(201).send()
+  const { title, description, duration } = req.body
+  database.create({ title, description, duration })
+  return reply.status(201).send()
 })
 
 server.get('/videos', (req, reply) => {
-    const { search } = req.query
-
-    const videos = database.list(search)
-
-    return reply.send(videos)
+  const { search } = req.query
+  const videos = database.list(search)
+  return reply.send(videos)
 })
 
 server.put('/videos/:id', (req, reply) => {
-    const { id } = req.params
-    const { title, description, duration } = req.body
-
-    database.update(id, { title, description, duration })
-
-    return reply.status(204).send()
+  const { id } = req.params
+  const { title, description, duration } = req.body
+  database.update(id, { title, description, duration })
+  return reply.status(204).send()
 })
 
 server.delete('/videos/:id', (req, reply) => {
-    const { id } = req.params
-
-    database.delete(id)
-
-    return reply.status(204).send()
+  const { id } = req.params
+  database.delete(id)
+  return reply.status(204).send()
 })
 
-server.listen({ port: 3333 }, () => {
-    console.log('Servidor rodando em http://localhost:3333')
+server.listen({ port: 3333 }, (err, address) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`✅ Servidor rodando em ${address}`)
 })
+
